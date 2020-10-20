e4248762d8215fadce5b50369dd1d84277fa4691
Creator5923 <nlokhande5923@gmail.com>
2020-10-20 10:45:46 -0700

    templates added

--- src/components/DataFiles/Image.js
+++ src/components/DataFiles/Image.js
@@ -7,49 +7,49 @@ const certImages = [
     },
     {
         id:2,
-        src:"Images/certificate-1.jpg",
+        src:"Images/certificate-2.png",
         alt:"certificte-1"
 
     },
     {
         id:3,
-        src:"Images/certificate-1.jpg",
+        src:"Images/certificate-3.png",
         alt:"certificte-1"
 
     },
     {
         id:4,
-        src:"Images/certificate-1.jpg",
+        src:"Images/certificate-4.jpg",
         alt:"certificte-1"
 
     },
     {
         id:5,
-        src:"Images/certificate-1.jpg",
+        src:"Images/certificate-5.jpg",
         alt:"certificte-1"
 
     },
     {
         id:6,
-        src:"Images/certificate-1.jpg",
+        src:"Images/certificate-6.jpg",
         alt:"certificte-1"
 
     },
     {
         id:7,
-        src:"Images/certificate-1.jpg",
+        src:"Images/certificate-7.jpg",
         alt:"certificte-1"
 
     },
     {
         id:8,
-        src:"Images/certificate-1.jpg",
+        src:"Images/certificate-8.jpg",
         alt:"certificte-1"
 
     },
     {
         id:9,
-        src:"Images/certificate-1.jpg",
+        src:"Images/certificate-9.png",
         alt:"certificte-1"
 
     }
