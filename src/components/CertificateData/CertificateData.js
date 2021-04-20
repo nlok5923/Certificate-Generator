5d478b4a1c3e572a380558e1535056d3d0b026e0
Nitanshu Lokhande <56120084+nlok5923@users.noreply.github.com>
2021-04-20 23:04:12 +0530

    Update CertificateData.js

--- src/components/CertificateData/CertificateData.js
+++ src/components/CertificateData/CertificateData.js
@@ -6,19 +6,11 @@ import "./CertificateData.css"
         const {name,body,date,sbody1 ,sbody2}=this.props
         return (
             <div className="data">
-                {/* <h2>{name}</h2>
-                <h1>{date}</h1>
-                <p>{body}</p>
-                <h2 className="sg1">{sbody1}</h2>
-                <h2 className="sg2" >{sbody2}</h2> 
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes b
-                */}
                 <h2 className="name">{name}</h2>
                 <h1 className="date">{date}</h1>
                 <p>{body}</p>
                 <h2 className="sg1">{sbody1}</h2>
-                <h2 className="sg2" >{sbody2}</h2> 
-                
+                <h2 className="sg2" >{sbody2}</h2>
              </div>
         )
     }
